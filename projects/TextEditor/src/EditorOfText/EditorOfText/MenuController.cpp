aed79e214ed763ab4d3027d4fef4d4cc22528802
Isaac Travers <isaac.a.travers@gmail.com>
2019-09-04 20:07:03 -0700

    Finished Basic Menu Popup Class
    We can now popup file, edit, view, tools, and help menus
    This finishes Issue #8 and continues issue #2
    
    Signed-off-by: Isaac Travers <isaac.a.travers@gmail.com>

--- projects/TextEditor/src/EditorOfText/EditorOfText/MenuController.cpp
+++ projects/TextEditor/src/EditorOfText/EditorOfText/MenuController.cpp
@@ -5,25 +5,149 @@
 *********************************************************************************/
 #include "MenuController.h"
 
-// Constructor
+//Used to keep track of menu windows and menu panels
+WINDOW* menuWindows[MENU_NUM_ITEMS];		/* A Menu Window */
+PANEL* menuPanels[MENU_NUM_ITEMS];			/* A Menu Panel  */
+
+/*
+	Default instructor doesn't need to do anything.
+*/
 MenuController::MenuController() {
-	//menuWindow = new WINDOW;
-	popupMenu(10, 12);
+
 }
 
-// Draw the Menu
+/*
+	Initializes all the menu windows and panels
+*/
+MenuController::MenuController(WINDOW* mainWindow, int numRows, int numCols) {
+	for (int i = 0; i < MENU_NUM_ITEMS; i++) {
+		
+		WINDOW* newWin = nullptr;
+		newWin = newwin(menuHeight, menuWidth, 2, ((numCols / MENU_ITEM_RATIO) * i) + XOFFSET);
+		menuWindows[i] = newWin;
+
+		//set a color box around each menuWindow
+		colorbox(menuWindows[i], COLOR_MENU_PAIR, 1);
+
+		//Set up the panels for the menu
+		menuPanels[i] = new_panel(menuWindows[i]);
+
+		//Setup the labels for each menu
+		mvwaddstr(menuWindows[i], 0, XOFFSET, menuItems[i].c_str());
+
+		//start the menus in hidden configuration
+		hide_panel(menuPanels[i]);
+	}
+}
+
+/*
+	Draws the menu items.
+*/
 void MenuController::drawMenu(int numRows , int numCols) {
-		//loop through and print menu items
-		attron(COLOR_PAIR(COLOR_TITLE_PAIR));	//Set The Color For characters we are printing with.
+		//loop through and print main menu items
+		attron(COLOR_PAIR(COLOR_MENU_PAIR));	//Set The Color For characters we are printing with.
 		for (int i = 0; i <= menuItems->length(); i++) {
 			mvaddstr(0, ((numCols / MENU_ITEM_RATIO) * i) + XOFFSET, menuItems[i].c_str());
 		}
-		attroff(COLOR_PAIR(COLOR_TITLE_PAIR)); //Unset the color
+		attroff(COLOR_PAIR(COLOR_MENU_PAIR)); //Unset the color
+
+		//refresh all the menuWindows
+		for (int i = 0; i < MENU_NUM_ITEMS; i++) {
+			wrefresh(menuWindows[i]);
+		}
+
+		//update all the panels stacks
+		update_panels();
+
+		//draw the screen
+		doupdate();
 }
 
-void MenuController::popupMenu(int numRows, int numCols) {
-	//menuWindow = new WINDOW;
-	//WINDOW* menuWindow;
-	//menuWindow = newwin(numRows, numCols, 1, 1); //create a new menuWindow
-	//box(menuWindow, 0, 0);
+/*
+	Closes all the menus
+*/
+void MenuController::closeAll() {
+	//loop through and close all the menu windows
+	for (int i = 0; i < MENU_NUM_ITEMS; i++) {
+		hide_panel(menuPanels[i]);
+		wrefresh(menuWindows[i]);
+	}
+
+	update_panels();
+	doupdate();
+}
+
+/*
+	Hides a specific menu
+*/
+void MenuController::closeMenu(int menuNum) {
+	hide_panel(menuPanels[menuNum]);
+	wrefresh(menuWindows[menuNum]);
+	update_panels();
+}
+
+/*
+	Causes a given menu to pop up
+*/
+void MenuController::popupMenu(int menuNum) {
+	//hide all panels
+	for (int i = 0; i < MENU_NUM_ITEMS; i++) {
+		hide_panel(menuPanels[i]);
+		update_panels();
+	}
+
+	show_panel(menuPanels[menuNum]);
+	wrefresh(menuWindows[menuNum]);
+}
+
+/*
+	This function was not written by me. I adapted it from the colorbox function in tui.c
+	that the teacher supplied.
+*/
+void MenuController::colorbox(WINDOW* win, chtype color, int hasbox)
+{
+	int maxy;
+#ifndef PDCURSES
+	int maxx;
+#endif
+	chtype attr = color & A_ATTR;  /* extract Bold, Reverse, Blink bits */
+
+	setcolor(win, color);
+
+#ifdef A_COLOR
+	if (has_colors())
+		wbkgd(win, COLOR_PAIR(color & A_CHARTEXT) | (attr & ~A_REVERSE));
+	else
+#endif
+		wbkgd(win, attr);
+
+	werase(win);
+
+#ifdef PDCURSES
+	maxy = getmaxy(win);
+#else
+	getmaxyx(win, maxy, maxx);
+#endif
+	if (hasbox && (maxy > 2))
+		box(win, 0, 0);
+
+	touchwin(win);
+	wrefresh(win);
+}
+
+/*
+	This function was not written by me. I adapted it from the setcolor
+	function in tui.c, supplied by the teacher.
+*/
+void MenuController::setcolor(WINDOW* win, chtype color)
+{
+	chtype attr = color & A_ATTR;  /* extract Bold, Reverse, Blink bits */
+
+#ifdef A_COLOR
+	attr &= ~A_REVERSE;  /* ignore reverse, use colors instead! */
+	wattrset(win, COLOR_PAIR(color & A_CHARTEXT) | attr);
+#else
+	attr &= ~A_BOLD;     /* ignore bold, gives messy display on HP-UX */
+	wattrset(win, attr);
+#endif
 }
--- projects/TextEditor/src/EditorOfText/EditorOfText/MenuController.h
+++ projects/TextEditor/src/EditorOfText/EditorOfText/MenuController.h
@@ -5,20 +5,32 @@
 *********************************************************************************/
 #ifndef MENU_CONTROLLER
 #define MENU_CONTROLLER
+#define A_ATTR (A_ATTRIBUTES)
 
 /* Defines the fraction of the
    window a single memory item takes.
    number is the inverse. */
 #define MENU_ITEM_RATIO 10
+#define MENU_NUM_ITEMS 5	/* Number of Menu Items */
+#define MENU_HEIGHT 10		/* Menu Height */
+#define MENU_WIDTH 20		/* Menu Width  */
+#define MENU_FILE 0
+#define MENU_EDIT 1
+#define MENU_VIEW 2
+#define MENU_TOOLS 3
+#define MENU_HELP 4
 
 /* Number of characters to offset the menu from the right of the screen. */
 #define XOFFSET 1
 
 #include <string>
+#include <vector>
 #include "curses.h"
+#include "panel.h"
 #include "customcolors.h"
 
 using std::string;
+using std::vector;
 
 /*******************************************************************************
  * Class Name:   MenuController
@@ -31,7 +43,9 @@ public:
 	 * Constructor
 	 * Parameters:
 	 *******************************************************************************/
+	
 	MenuController();
+	MenuController(WINDOW*, int, int);
 
 	/*******************************************************************************
 	 * drawMenu(int, int)
@@ -40,15 +54,24 @@ public:
 	 *******************************************************************************/
 	void drawMenu(int, int);
 
-	void popupMenu(int, int);
-	void closeMenu();
+	void popupMenu(int);
+	void closeMenu(int);
+	void closeAll();
+	void colorbox(WINDOW* win, chtype color, int hasbox);
+	void setcolor(WINDOW* win, chtype color);
 
 private:
 	/* A List of the items we will have in our menu. */
-	string menuItems[5] = { "File", "Edit", "View", "Tools", "Help" };
+	string menuItems[MENU_NUM_ITEMS] = { "File", "Edit", "View", "Tools", "Help" };
 
-	/* A Menu Window */
+	//WINDOW* menuWindows[MENU_NUM_ITEMS];		/* A Menu Window */
+	//WINDOW** menuWindows;
+	//vector<WINDOW*> menuWindows;
 	
+
+	/* The menu dimensions */
+	const int menuHeight = MENU_HEIGHT;
+	const int menuWidth = MENU_WIDTH;
 };
 
 #endif
